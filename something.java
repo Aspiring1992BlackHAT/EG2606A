bf383a2f68c8161e96b9a15a3c9dff103fca02f4
Aspiring1992BlackHAT <a0074413@gmail.com>
2013-08-25 01:31:33 +0800

    3rd line added to something.java

--- something.java
+++ something.java
@@ -4,7 +4,8 @@ Public class something
  Public static void main(String args[])
 {
  System.out.println("I am now live on GitHub!!, hurray!!");
- System.out.printf("Let's get that 2 Mc's");
+ System.out.printf("Let's get that 2 Mc's\n");
+ System.out.printf("Lets get it started in here!!");
 
 }
 
